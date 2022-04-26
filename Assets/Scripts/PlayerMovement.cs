13e42a91f5d624888bcd20852447928c8bbebb45
Matías Arocena <m.arocena97@gmail.com>
2022-04-26 19:18:54 -0300

    Move jam folder to root

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,17 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 7f;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         float movement = Input.GetAxisRaw("Horizontal");
         transform.position += new Vector3(movement * speed * Time.deltaTime, 0, 0);
+       
     }
 }
