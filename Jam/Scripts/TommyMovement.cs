444ebcd8f1d95ec21298d9eac0829210ee59d02f
martingomez8 <martingomez169@gmail.com>
2022-04-26 19:14:00 -0300

    holiss

---
+++ Jam/Scripts/TommyMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TommyMovement : MonoBehaviour
+{
+    public float Speed;
+    public float JumpForce;
+    public GameObject BulletPrefab;
+
+    private Rigidbody2D Rigidbody2D;
+    private Animator Animator;
+    private float Horizontal;
+    private bool Grounded;
+    bool jump;
+   
+
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Jump();
+        }
+
+
+        Animator.SetBool("Running", Horizontal != 0.0f);
+
+        if (Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+        //Animator.SetBool("running", Horizontal != 0.0f);
+
+    }
+    private void Jump()
+    {
+            Rigidbody2D.AddForce(Vector2.up * JumpForce);
+            Animator.SetBool("Jumping", jump);
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody2D.velocity = new Vector2(Horizontal * Speed, Rigidbody2D.velocity.y);
+    }
+}
